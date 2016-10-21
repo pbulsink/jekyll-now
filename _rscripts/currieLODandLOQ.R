dbdb6d6ac26c65636ea76f8f3ee3d76b5c20f54e
Philip Bulsink <philip.bulsink@uottawa.ca>
2016-10-21 14:36:32 -0400

    Work on LOD/LOQ

--- _rscripts/currieLODandLOQ.R
+++ _rscripts/currieLODandLOQ.R
@@ -1,14 +1,11 @@
-### Functions for Currie estimation of LOD/LOQ From Rajakovic et. al., Talanta 102 (2012) 79-87. doi:10.1016/j.talanta.2012.08.016 and Currie, Anal.
-### Chem. 40 (1968) 586-593 and Currie, Pure Appl. Chem. 67 (1995) 1699-1723 by Philip Bulsink
+### Functions for Currie estimation of LOD/LOQ From Rajakovic et. al.,
+### Talanta 102 (2012) 79-87. doi:10.1016/j.talanta.2012.08.016 and Currie,
+### Anal.  Chem. 40 (1968) 586-593 and Currie, Pure Appl. Chem. 67 (1995)
+### 1699-1723 by Philip Bulsink
 
 variance <- function(x) {
     # Sum (i=1..n) (xi-xbar)^2
-    mean.x <- mean(x)
-    variance <- 0
-    for (i in c(1:length(x))) {
-        var.x.sum <- variance + (x[i] - mean.x)^2
-    }
-    return(variance)
+    return((x - mean(x))^2)
 }
 
 intercept.sd <- function(x, y) {
@@ -16,33 +13,49 @@ intercept.sd <- function(x, y) {
     return(regression.sd(x, y) * ((1/length(x) + ((mean(x)^2)/variance(x)))))
 }
 
-regression.sd <- function(y, x) {
+regression.sd <- function(x, y) {
     # s_y/x
-    return()
+    fit <- lm(y ~ x)
+    a <- fit$coefficients[[1]]
+    b <- fit$coefficients[[2]]
+    return(sqrt(((y - a - b * x)^2)/(length(x) - 2)))
 }
 
 eta <- function(M, x) {
     return((1/M) + (1/length(x)) + ((mean(x)^2)/(variance(x))))
 }
 
-eta.root <- function(eta) {
-    return(sqrt(eta))
+eta.root <- function(M, x) {
+    return(sqrt(eta(M, x)))
 }
 
-concentration.lod <- function() {
+concentration.lod <- function(x, y, M = 1, falsePositive = 0.95) {
+    tp <- qt(1 - 0.5 * (1 - falsePositive), df = (length(x) - 1))
+    return((tp * regression.sd(x, y) * eta.root(M, x))/(lm(y ~ x)$coefficients[[2]]))
 }
 
-concentration.loq <- function() {
+concentration.loq <- function(x, y, M = 1, falsePositive = 0.95, falseNegative = 0.99) {
+    tp <- qt(1 - 0.5 * (1 - falsePositive), df = (length(x) - 1))
+    tq <- qt(1 - 0.5 * (1 - falseNegative), df = (length(x) - 1))
+    return(((tp + tq) * regression.sd(x, y) * eta.root(M, x))/(lm(y ~ x)$coefficients[[2]]))
 }
 
-response.lod <- function() {
+response.lod <- function(x, y, M = 1, falsePositive = 0.95) {
+    tp <- qt(1 - 0.5 * (1 - falsePositive), df = (length(x) - 1))
+    return(tp * regression.sd(x, y) * eta.root(M, x))
 }
 
-response.loq <- function() {
+response.loq <- function(x, y, M = 1, falsePositive = 0.95, falseNegative = 0.99) {
+    tp <- qt(1 - 0.5 * (1 - falsePositive), df = (length(x) - 1))
+    tq <- qt(1 - 0.5 * (1 - falseNegative), df = (length(x) - 1))
+    return((tp + tq) * regression.sd(x, y) * eta.root(M, x))
 }
 
-calculateCurrieLimits <- function(cal, falsePositive = 0.95, falseNegative = 0.99) {
-    # input: list(x=..., y-...) with x, y values of calibration.  compoundname, stock calibration, dilutionfactor, response, dilutionfactor,response...
+calculateCurrieLimits <- function(cal, replicate_cals = 1, falsePositive = 0.95, 
+    falseNegative = 0.99) {
+    # input: list(x=..., y-...) with x, y values of calibration.
+    # compoundname, stock calibration, dilutionfactor, response,
+    # dilutionfactor,response...
 }
 
 inputCurrieData <- function(dataplace) {
