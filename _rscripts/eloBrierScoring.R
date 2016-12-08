f0460ec2f036da0b2be92f999b4948954344bf84
Phil OSx <bulsinkp@gmail.com>
2016-12-08 09:32:18 -0500

    Scoring Work

--- _rscripts/eloBrierScoring.R
+++ _rscripts/eloBrierScoring.R
@@ -6,24 +6,6 @@ require(optimx)
 require(doParallel)
 require(MLmetrics)
 
-#' Calculates and returns brier score
-#'
-#' @param predicted A vector or data frame of predictions (0..1)
-#' @param results A vector or data frame of results (0,1)
-#' @return a number representing the binary Brier score. 0 is perfect prediction
-brier<-function(predicted, results){
-    return(sum((predicted-results)^2)/nrow(predicted))
-}
-
-#' Calculates and returns adjbrier score
-#'
-#' @param predicted A vector or data frame of predictions (0..1)
-#' @param results A vector or data frame of results (0,1)
-#' @return a number representing the binary Brier score. 0 is perfect prediction
-adjBrier<-function(predicted, results){
-    return(list('score'=apply((predicted-results)^2, 1, sum), 'max'=nrow(predicted)))
-}
-
 #' Calculates odds of each outcome and tabulates results
 #'
 #' @param eloHist EloHistory covering all days of games in schedule
@@ -43,10 +25,10 @@ getPredictedResults<-function(eloHist, schedule,pWin,pLoss){
         elo_diff<-tail(eloHist[eloHist$Date < schedule[g, 'Date'],home],1)-tail(eloHist[eloHist$Date < schedule[g, 'Date'],visitor],1)
 
         #Results
-        if (schedule[g,'Result'] == 0){
+        if (schedule[g,'Result'] < 0.4){
             results[g]<-1
         }
-        else if (schedule[g,'Result'] == 1){
+        else if (schedule[g,'Result'] > 0.6){
             results[g]<-3
         }
         else{
@@ -60,42 +42,32 @@ getPredictedResults<-function(eloHist, schedule,pWin,pLoss){
     return(list('predicted'=predicted, 'results'=results))
 }
 
-#'one call script to calculate brier score. Calculates predicted odds, results, brier
-#'
-#' @param eloHist EloHistory covering all days of games in schedule
-#' @param schedule All games to calculate brier score over
-#' @param pWin pWin logit fit
-#' @param pLoss pLoss logit fit
-#' @return a single numerical brier score
-seasonBrierScore<-function(eloHist, schedule, pWin, pLoss){
-    pr<-getPredictedResults(eloHist, schedule, pWin, pLoss)
-    predicted<-pr$predicted
-    results<-pr$results
-    return(brier(predicted, results))
-}
-
-#'one call script to calculate adjusted brier score. Calculates predicted odds, results, brier
+#'one call script to calculate scores score. Calculates predicted odds, results, returns list of brier, LL scores
 #'
 #' @param eloHist EloHistory covering all days of games in schedule
 #' @param schedule All games to calculate brier score over
 #' @param pWin pWin logit fit
 #' @param pLoss pLoss logit fit
 #' @return a single numerical brier score. 0 is best, 1 is worst
-seasonBrierAdjScore<-function(eloHist, schedule, pWin, pLoss){
+seasonScore<-function(eloHist, schedule, pWin, pLoss){
     pr<-getPredictedResults(eloHist, schedule, pWin, pLoss)
     predicted<-pr$predicted
     results<-pr$results
-    return(sum(calcscore(results~predicted$VWin+predicted$D+predicted$HWin, bounds=c(0,1)))/nrow(predicted))
-}
-
-seasonLogLossScore<-function(eloHist, schedule, pWin, pLoss){
-    pr<-getPredictedResults(eloHist, schedule, pWin, pLoss)
-    predicted<-pr$predicted
-    results<-pr$results
+    message('multibrier')
+    multiBrier<-sum(calcscore(results~predicted$VWin+predicted$D+predicted$HWin, bounds=c(0,1)))/nrow(predicted)
     results[results == 1]<-'VWin'
-    results[results == 3]<-'D'    
+    results[results == 2]<-'D'
     results[results == 3]<-'HWin'
-    return(MultiLogLoss(y_true=results, y_pred=predicted))
+    pre<-predicted$HWin/(predicted$HWin+predicted$VWin)
+    res<-rep(0, length(results))
+    res[results == 'HWin']<-1
+    message('multiLL')
+    multiLL<-MultiLogLoss(y_true=results, y_pred=predicted)
+    message('binll')
+    binLL<-LogLoss(y_true=res, y_pred=pre)
+    message('binbrier')
+binBrier<-sum(unlist(brierscore(res~pre)))/length(pre)
+    return(list('multiLL'=multiLL, 'binLL'=binLL, 'multiBrier'=multiBrier, 'binBrier'=binBrier))
 }
 
 scoreEloVar<-function(p=c('kPrime'=10, 'gammaK'=1), regressStrength=3, homeAdv=0, newTeam=1300, nhl_data){
@@ -117,65 +89,18 @@ scoreEloVar<-function(p=c('kPrime'=10, 'gammaK'=1), regressStrength=3, homeAdv=0
     message(paste0('pWin at even: ', pw))
 
     #Calculate Brier Score
-    message("Calculating Brier Score")
+    message("Calculating Scores Score")
     elo16<-elo$Ratings[(elo$Ratings$Date>as.Date("2015-08-01") & elo$Ratings$Date<as.Date("2016-08-01")),]
     nhl16<-nhl_data[(nhl_data$Date > as.Date("2015-08-01") & nhl_data$Date < as.Date("2016-08-01")),]
-    return(seasonBrierAdjScore(elo16, schedule = nhl16, pWin, pLoss))
+    s<-seasonScore(elo16, schedule = nhl16, pWin, pLoss)
+    message(paste0("LLMulti: ", s['multiLL'], " LLBinary: ", s['binLL'], " BrierMulti: ", s['multiBrier'], " BrierBinary: ", s['binBrier']))
+    scores<-list('kPrime'=kPrime, 'gammaK'=gammaK, 'multiLL'=s['multiLL'], 'binLL'=s['binLL'], 'multiBrier'=s['multiBrier'], 'binBrier'=s['binBrier'])
+    return(scores)
 
-}
-
-logLossScoreEloVar<-function(p=c('kPrime'=10, 'gammaK'=1), regressStrength=3, homeAdv=0, newTeam=1300, nhl_data){
-    #Calculate Elo by parameter
-    kPrime=p[1]
-    gammaK=p[2]
-    #regressStrength=p[3]
-    #homeAdv=p[4]
-    #newTeam=1500-4*p[5] # Thus, as p5 goes up, newteam ratings go down. Typically p[5] = 50, newTeam=1300
-    message(paste0("Calculating Elo with kPrime=",kPrime," gammaK=",gammaK," newTeam=",newTeam," regStrength=",regressStrength," homeAdv=",homeAdv))
-    elo<-calculateEloRatings(nhl_data, k=kPrime, gammaK = gammaK, new_teams = newTeam, regress_strength = regressStrength, home_adv = homeAdv, k_var = TRUE, meta=FALSE)
-    
-    #Calculate pWin, pLoss
-    message("Calculating pWin, pLoss")
-    pResults<-pResCalc(elo$Ratings, nhl_data)
-    pWin<-pResults$pWin
-    pLoss<-pResults$pLoss
-    pw<-predict(pWin, data.frame("EloDiff"=0), type='response')
-    message(paste0('pWin at even: ', pw))
-    
-    #Calculate LogLoss Score
-    message("Calculating Brier Score")
-    elo16<-elo$Ratings[(elo$Ratings$Date>as.Date("2015-08-01") & elo$Ratings$Date<as.Date("2016-08-01")),]
-    nhl16<-nhl_data[(nhl_data$Date > as.Date("2015-08-01") & nhl_data$Date < as.Date("2016-08-01")),]
-    return(seasonLogLossScore(elo16, schedule = nhl16, pWin, pLoss))
 }
 
 optEloVar<-function(nhl_data){
     return(optim(par=c(10, 1), fn=scoreEloVar, nhl_data=nhl_data))  #, lower=0, upper=100))
-}
-
-scoreEloVar2<-function(kPrime=10, regressStrength=3, homeAdv=0, newTeam=1300, nhl_data){
-    #Calculate Elo by parameter
-    message(paste0("Calculating Elo with kPrime=",kPrime," newTeam=",newTeam," regStrength=",regressStrength," homeAdv=",homeAdv))
-    elo<-calculateEloRatings(nhl_data, k=kPrime, new_teams = newTeam, regress_strength = regressStrength, home_adv = homeAdv, meta=FALSE)
-
-    #Calculate pWin, pLoss
-    message("Calculating pWin, pLoss")
-    pResults<-pResCalc(elo$Ratings, nhl_data)
-    pWin<-pResults$pWin
-    pLoss<-pResults$pLoss
-    pw<-predict(pWin, data.frame("EloDiff"=0), type='response')
-    message(paste0('pWin at even: ', pw))
-
-    #Calculate Brier Score
-    message("Calculating Brier Score")
-    elo16<-elo$Ratings[(elo$Ratings$Date>as.Date("2015-08-01") & elo$Ratings$Date<as.Date("2016-08-01")),]
-    nhl16<-nhl_data[(nhl_data$Date > as.Date("2015-08-01") & nhl_data$Date < as.Date("2016-08-01")),]
-    return(seasonBrierAdjScore(elo16, schedule = nhl16, pWin, pLoss))
-
-}
-
-optEloVar2<-function(nhl_data){
-    return(optim(par=c(10, 1, 3, 30, 50), fn=scoreEloVar2, nhl_data=nhl_data))  #, lower=0, upper=100))
 }
 
 pResCalc<-function(elo, nhl_data){
@@ -203,9 +128,9 @@ pResCalc<-function(elo, nhl_data){
     for (i in unique(round(nhl_data$EloDiff))){
         propresults$EloDiff<-c(propresults$EloDiff, i)
         x<-nhl_data[round(nhl_data$EloDiff) == i,]
-        propresults$Win<-c(propresults$Win, length(x[x$Result == 1,'Result']))
-        propresults$Draw<-c(propresults$Draw, length(x[(x$Result < 0.61 & x$Result > 0.39),'Result']))
-        propresults$Loss<-c(propresults$Loss, length(x[x$Result == 0,'Result']))
+        propresults$Win<-c(propresults$Win, length(x[x$Result>0.6,'Result']))
+        propresults$Draw<-c(propresults$Draw, length(x[(x$Result <= 0.6 & x$Result >= 0.4),'Result']))
+        propresults$Loss<-c(propresults$Loss, length(x[x$Result < 0.4, 'Result']))
     }
     propresults<-as.data.frame(propresults)
 
@@ -222,10 +147,10 @@ pResCalc<-function(elo, nhl_data){
 }
 
 eloVarPlotData<-function(nhl_data){
-    cl <- makeCluster(3, outfile="./stdout.log")
+    cl <- makeCluster(6, outfile="./stdout.log")
     registerDoParallel(cl)
-    exportFuns<-c('scoreEloVar2', 'calculateEloRatings', 'pResCalc', 'seasonBrierAdjScore', 'seasonBrierScore', 'splitDates', '.eloSeason', 'predictEloResult', 'newRankings', 'variableK', 'getPredictedResults')
-    scores<-foreach(i=1:150, .export=exportFuns, .packages = c('scoring', 'reshape2')) %dopar% scoreEloVar2(k=i, regressStrength=3, homeAdv=0, newTeam=1300, nhl_data)
+    exportFuns<-c('scoreEloVar', 'seasonScore', 'calculateEloRatings', 'pResCalc', 'splitDates', '.eloSeason', 'predictEloResult', 'newRankings', 'variableK', 'getPredictedResults')
+    scores<-foreach(i=0:100, .export=exportFuns, .packages = c('scoring', 'reshape2', 'MLmetrics')) %:% foreach(j=0:30) %dopar% scoreEloVar(p=c(i/2, j/10), regressStrength=3, homeAdv=0, newTeam=1300, nhl_data)
 
     stopCluster(cl)
 
