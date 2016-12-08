e7500ffc2fdf6ffb69aeb7903e098a31726bbc81
Philip Bulsink <philip.bulsink@uottawa.ca>
2016-12-08 14:56:16 -0500

    Many-scoring methods testing of multiple methods

--- _rscripts/eloBrierScoring.R
+++ _rscripts/eloBrierScoring.R
@@ -17,29 +17,51 @@ getPredictedResults<-function(eloHist, schedule,pWin,pLoss){
     #For recent Elo, we have results of 0, 0.25, 0.4, 0.6, 0.75, 1
     #Coded as VWin, Draw ,HWin
     ngames<-nrow(schedule)
-    predicted<-data.frame('VWin'=rep(0,ngames), 'D'=rep(0), 'HWin'=rep(0))
-    results<-rep(0, ngames)
+    predicted<-data.frame('VWin'=rep(0,ngames), 'VOT'=rep(0), 'VSO'=rep(0), 'HSO'=rep(0), 'HOT'=rep(0), 'HWin'=rep(0))
+    resultsNum<-rep(0, ngames)
+    resultsName<-rep('', ngames)
     for(g in c(1:ngames)){
         home<-make.names(as.character(schedule[g,'Home']))
         visitor<-make.names(as.character(schedule[g,'Visitor']))
         elo_diff<-tail(eloHist[eloHist$Date < schedule[g, 'Date'],home],1)-tail(eloHist[eloHist$Date < schedule[g, 'Date'],visitor],1)
 
         #Results
-        if (schedule[g,'Result'] < 0.4){
-            results[g]<-1
+        if (schedule[g,'Result'] == 0){
+            resultsNum[g]<-1
+            resultsName[g]<-'VWin'
         }
-        else if (schedule[g,'Result'] > 0.6){
-            results[g]<-3
+        else if (schedule[g,'Result'] == 0.25){
+            resultsNum[g]<-2
+            resultsName[g]<-'HOT'
         }
-        else{
-            results[g]<-2
+        else if (schedule[g,'Result'] == 0.4){
+            resultsNum[g]<-3
+            resultsName[g]<-'VSO'
+        }
+        else if (schedule[g,'Result'] == 0.6){
+            resultsNum[g]<-4
+            resultsName[g]<-'HSO'
+        }
+        else if (schedule[g,'Result'] == 0.75){
+            resultsNum[g]<-5
+            resultsName[g]<-'HOT'
+        }
+        else if (schedule[g,'Result'] == 1){
+            resultsNum[g]<-6
+            resultsName[g]<-'HWin'
         }
         #Predictions
         predicted[g,'VWin']<-v<-predict(pLoss, data.frame("EloDiff"=elo_diff), type='response')
         predicted[g,'HWin']<-h<-predict(pWin, data.frame("EloDiff"=elo_diff), type='response')
+        #Draw Sorting (64% OT/36% SO)
+        d<-1-(h+v)
+        predicted[g,'VOT']<-d*0.64*v
+        predicted[g,'VSO']<-d*0.36*v
+        predicted[g,'HSO']<-d*0.36*h
+        predicted[g,'HOT']<-d*0.64*h
+        
     }
-    predicted$D<-1-(predicted$VWin+predicted$HWin)
-    return(list('predicted'=predicted, 'results'=results))
+    return(list('predicted'=predicted, 'resultsNum'=resultsNum, 'resultsName'=resultsName))
 }
 
 #'one call script to calculate scores score. Calculates predicted odds, results, returns list of brier, LL scores
@@ -52,22 +74,53 @@ getPredictedResults<-function(eloHist, schedule,pWin,pLoss){
 seasonScore<-function(eloHist, schedule, pWin, pLoss){
     pr<-getPredictedResults(eloHist, schedule, pWin, pLoss)
     predicted<-pr$predicted
-    results<-pr$results
-    message('multibrier')
-    multiBrier<-sum(calcscore(results~predicted$VWin+predicted$D+predicted$HWin, bounds=c(0,1)))/nrow(predicted)
-    results[results == 1]<-'VWin'
-    results[results == 2]<-'D'
-    results[results == 3]<-'HWin'
+    results<-pr$resultsNum
+    rName<-resName<-pr$resultsName
+    
+    message('multi6')
+    multiBrier6<-sum(calcscore(results~predicted$VWin+predicted$VOT+predicted$VSO+predicted$HSO+predicted$HOT+predicted$HWin, bounds=c(0,1)))/nrow(predicted)
+    multiLL6<-MultiLogLoss(y_true=rName, y_pred=predicted)
+    
+    message('multiWinDraw')
+    res<-rep(0, length(results))
+    res[results == 1]<-'1'
+    res[results >= 2 & results <=5]<-'2'
+    res[results == 6]<-'3'
+    predicted$D<-predicted$VOT+predicted$VSO+predicted$HSO+predicted$HOT
+    resName[resName %in% c('VOT', 'VSO', 'HSO', 'HOT')]<-'D'
+    pre<-predicted[,c('VWin','D', 'HWin')]
+    pre$VWin<-predicted$VWin/(predicted$VWin+predicted$D+predicted$HWin)
+    pre$D<-predicted$D/(predicted$VWin+predicted$D+predicted$HWin)
+    pre$HWin<-predicted$HWin/(predicted$VWin+predicted$D+predicted$HWin)
+    multiBrierWinDraw<-sum(calcscore(res~pre$VWin+pre$D+pre$HWin, bounds=c(0,1)))/nrow(predicted)
+    multiLLWinDraw<-MultiLogLoss(y_true=resName, y_pred=pre)
+    
+    message('multiWinOTDraw')
+    res<-rep(0, length(results))
+    res[results <= 2]<-'1'
+    res[results >= 3 & results <=4]<-'2'
+    res[results >= 5]<-'3'
+    predicted$D<-predicted$VSO+predicted$HSO
+    pre$VWin<-predicted$VWin/(predicted$VWin+predicted$D+predicted$HWin)
+    pre$D<-predicted$D/(predicted$VWin+predicted$D+predicted$HWin)
+    pre$HWin<-predicted$HWin/(predicted$VWin+predicted$D+predicted$HWin)
+    multiBrierWinOTDraw<-sum(calcscore(res~pre$VWin+pre$D+pre$HWin, bounds=c(0,1)))/nrow(predicted)
+    resName<-rName
+    resName[resName %in% c('VSO', 'HSO')]<-'D'
+    multiLLWinOTDraw<-MultiLogLoss(y_true=resName, y_pred=pre)
+    
+    
+    message('binaryScore')
     pre<-predicted$HWin/(predicted$HWin+predicted$VWin)
     res<-rep(0, length(results))
-    res[results == 'HWin']<-1
-    message('multiLL')
-    multiLL<-MultiLogLoss(y_true=results, y_pred=predicted)
-    message('binll')
+    res[results %in% c('HWin', 'HOT', 'HSO')]<-1
     binLL<-LogLoss(y_true=res, y_pred=pre)
-    message('binbrier')
-binBrier<-sum(unlist(brierscore(res~pre)))/length(pre)
-    return(list('multiLL'=multiLL, 'binLL'=binLL, 'multiBrier'=multiBrier, 'binBrier'=binBrier))
+    binBrier<-sum(unlist(brierscore(res~pre)))/length(pre)
+    
+    return(data.frame('multiBrier6'=multiBrier6, 'multiLL6'=multiLL6, 
+                'multiBrierWinDraw'=multiBrierWinDraw, 'multiLLWinDraw'=multiLLWinDraw, 
+                'multiBrierWinOTDraw'=multiBrierWinOTDraw, 'multiLLWinOTDraw'=multiLLWinOTDraw,
+                'binBrier'=binBrier, 'binLL'=binLL))
 }
 
 scoreEloVar<-function(p=c('kPrime'=10, 'gammaK'=1), regressStrength=3, homeAdv=0, newTeam=1300, nhl_data){
@@ -93,8 +146,8 @@ scoreEloVar<-function(p=c('kPrime'=10, 'gammaK'=1), regressStrength=3, homeAdv=0
     elo16<-elo$Ratings[(elo$Ratings$Date>as.Date("2015-08-01") & elo$Ratings$Date<as.Date("2016-08-01")),]
     nhl16<-nhl_data[(nhl_data$Date > as.Date("2015-08-01") & nhl_data$Date < as.Date("2016-08-01")),]
     s<-seasonScore(elo16, schedule = nhl16, pWin, pLoss)
-    message(paste0("LLMulti: ", s['multiLL'], " LLBinary: ", s['binLL'], " BrierMulti: ", s['multiBrier'], " BrierBinary: ", s['binBrier']))
-    scores<-list('kPrime'=kPrime, 'gammaK'=gammaK, 'multiLL'=s['multiLL'], 'binLL'=s['binLL'], 'multiBrier'=s['multiBrier'], 'binBrier'=s['binBrier'])
+    message(paste0("LLM6: ", s[['multiLL6']], " LLWD: ", s[['multiLLWinDraw']], " LLWOTD: ", s[['multiLLWinOTDraw']]," LLBin: ", s[['binLL']],"BrierM6: ", s[['multiBrier6']], " BWD: ", s[['multiBrierWinDraw']], " BWOTD: ", s[['multiBrierWinOTDraw']]," BBin: ", s[['binBrier']]))
+    scores<-list('kPrime'=kPrime, 'gammaK'=gammaK, 'multiLL6'=s[['multiLL6']], 'multiLLWinDraw'=s[['multiLLWinDraw']], 'multiLLWinOTDraw'=s[['multiLLWinOTDraw']], 'binLL'=s[['binLL']], 'multiBrier6'=s[['multiBrier6']], 'multiBrierWinDraw'=s[['multiBrierWinDraw']], 'multiBrierWinOTDraw'=s[['multiBrierWinOTDraw']], 'binBrier'=s[['binBrier']])
     return(scores)
 
 }
@@ -147,12 +200,16 @@ pResCalc<-function(elo, nhl_data){
 }
 
 eloVarPlotData<-function(nhl_data){
-    cl <- makeCluster(6, outfile="./stdout.log")
+    cl <- makeCluster(7, outfile="./stdout.log")
     registerDoParallel(cl)
     exportFuns<-c('scoreEloVar', 'seasonScore', 'calculateEloRatings', 'pResCalc', 'splitDates', '.eloSeason', 'predictEloResult', 'newRankings', 'variableK', 'getPredictedResults')
-    scores<-foreach(i=0:100, .export=exportFuns, .packages = c('scoring', 'reshape2', 'MLmetrics')) %:% foreach(j=0:30) %dopar% scoreEloVar(p=c(i/2, j/10), regressStrength=3, homeAdv=0, newTeam=1300, nhl_data)
+    scores<-foreach(i=0:50, .combine='c', .export=exportFuns, .packages = c('scoring', 'reshape2', 'MLmetrics')) %:% foreach(j=0:20) %dopar% scoreEloVar(p=c(i/2, j/5), regressStrength=3, homeAdv=0, newTeam=1300, nhl_data)
 
     stopCluster(cl)
 
+    scores<-as.data.frame(apply(as.data.frame(do.call('rbind', scores)), 2, unlist))
+    
     return(scores)
+    
+    #ggplot(multiScores, aes(x=kPrime, y=gammaK, z=multiLL6, fill=multiLL6)) + geom_tile() + coord_equal() + geom_contour(color = "white", alpha = 0.5) + scale_fill_distiller(palette="Spectral", na.value="white") + theme_bw()
 }
