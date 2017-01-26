619f707750489c9cb01d5d65f8154a519edcddc9
Philip Bulsink <philip.bulsink@uottawa.ca>
2017-01-26 15:26:06 -0500

    Random ties, plus scoring like @IneffectiveMath

--- elo_season_predictions.R
+++ elo_season_predictions.R
@@ -12,23 +12,34 @@ predictSeasonElo<-function(elo_data, schedule, n_sims=10){
 
     results <- matrix(predictEloWins.vec(schedule$PHome, n_sims), ncol=n_sims, byrow = TRUE)
 
-    team_performance<-matrix(NA, nrow=length(nhl_teams), ncol=n_sims)
+    team_performance<-matrix(NA, nrow=length(nhl_teams), ncol=n_sims * 3)
     rownames(team_performance)<-make.names(nhl_teams)
     for(i in 1:n_sims){
         for(team in nhl_teams){
-            team_performance[make.names(team), i]<-sum(results[schedule$Home == team,i]==1) + sum(results[schedule$Visitor == team, i]==0)
+            team_performance[make.names(team), i]<-sum(results[schedule$Home == team,i]>0.5) + sum(results[schedule$Visitor == team, i]<0.5)
+            team_performance[make.names(team), i+n_sims]<-sum(results[schedule$Home == team, i]==0.4)+sum(results[schedule$Visitor == team, i]==0.4)
+            team_performance[make.names(team), i+2*n_sims]<-2 * team_performance[make.names(team), i] + team_performance[make.names(team), i+n_sims]
+
         }
     }
     team_results<-data.frame("Team"=nhl_teams)
-    team_results$Max<-apply(team_performance, 1, max)
-    team_results$Min<-apply(team_performance, 1, min)
-    team_results$Wins<-apply(team_performance, 1, mean)
-    team_results$SD<-apply(team_performance, 1, sd)
-
-    team_results$Points<-2*team_results$Wins
+    team_results$WinsMax<-apply(team_performance[,1:n_sims], 1, max)
+    team_results$WinsMin<-apply(team_performance[,1:n_sims], 1, min)
+    team_results$Wins<-apply(team_performance[,1:n_sims], 1, mean)
+    team_results$WinsSD<-apply(team_performance[,1:n_sims], 1, sd)
+    team_results$OTLoss<-apply(team_performance[,(n_sims+1):(2*n_sims)], 1, mean)
+    team_results$OTLossMax<-apply(team_performance[,(n_sims+1):(2*n_sims)], 1, mean)
+    team_results$OTLossMin<-apply(team_performance[,(n_sims+1):(2*n_sims)], 1, mean)
+    team_results$OTLossSD<-apply(team_performance[,(n_sims+1):(2*n_sims)], 1, mean)
+    team_results$PointsMax<-apply(team_performance[,(2*n_sims+1):(3*n_sims)], 1, max)
+    team_results$PointsMin<-apply(team_performance[,(2*n_sims+1):(3*n_sims)], 1, min)
+    team_results$Points<-apply(team_performance[,(2*n_sims+1):(3*n_sims)], 1, mean)
+    team_results$PointsSD<-apply(team_performance[,(2*n_sims+1):(3*n_sims)], 1, sd)
 
     team_results
 }
 
 predictEloResults.vec<-Vectorize(FUN = function(elo_diff, h_adv=0){return(1/(1 + (10^(((-1*elo_diff)+h_adv)/400))))},vectorize.args = 'elo_diff')
-predictEloWins.vec<-Vectorize(FUN = function(pWin, n_sims){return(sample(c(0,1), size = n_sims, replace = TRUE, prob=c(1-pWin, pWin)))},vectorize.args = 'pWin')
+predictEloWins.vec<-Vectorize(FUN = function(pWin, n_sims){return(sample(c(0,0.4, 0.6, 1), size = n_sims, replace = TRUE, prob=c(1-pWin-0.1, 0.05, 0.05, pWin-0.1)))},vectorize.args = 'pWin')
+
+sum(dnorm(x=p$Points-abs(p$Points-p$ActualPoints), mean = p$Points, sd=p$PointsSD))
